79532abc8634a4335fc72ffb733159371140d094
Sucerq <j.jjsr16@gmail.com>
2026-05-28 23:11:46 -0500

    Coreccion

--- src/entities/detalle_venta.py
+++ src/entities/detalle_venta.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Boolean, Column, DateTime, String,Numeric,ForeignKey
+from sqlalchemy import Boolean, Column, DateTime, String, Numeric, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
@@ -11,16 +11,18 @@ from src.database.config import Base
 class Detalle_Venta(Base):
     __tablename__ = "tbl_Detalle_Venta"
 
-    id_Detalle_Venta = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    id_Detalle_Venta = Column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
+    )
 
     fecha_creacion = Column(DateTime(timezone=True), server_default=func.now())
     fecha_edicion = Column(DateTime(timezone=True), onupdate=func.now())
 
     id_usuario_crea = Column(
-        UUID(as_uuid=True), ForeignKey("tbl_usuarios.id_Usuarios"), nullable=False
+        UUID(as_uuid=True), ForeignKey("tbl_usuarios.id_Usuario"), nullable=False
     )
     id_usuario_edita = Column(
-        UUID(as_uuid=True), ForeignKey("tbl_usuarios.id_Usuarios"), nullable=True
+        UUID(as_uuid=True), ForeignKey("tbl_usuarios.id_Usuario"), nullable=True
     )
 
     usuario_crea = relationship("Usuario", foreign_keys=[id_usuario_crea])
@@ -30,9 +32,7 @@ class Detalle_Venta(Base):
     id_Venta = Column(
         UUID(as_uuid=True), ForeignKey("tbl_Venta.id_Venta"), nullable=False
     )
-    id_Auto = Column(
-        UUID(as_uuid=True), ForeignKey("tbl_Auto.id_Auto"), nullable=False
-    )
+    id_Auto = Column(UUID(as_uuid=True), ForeignKey("tbl_Auto.id_Auto"), nullable=False)
     # --- Relationships ---
     Venta = relationship("Auto", foreign_keys=[id_Venta])
     Auto = relationship("Empleado", foreign_keys=[id_Auto])
--- src/entities/venta.py
+++ src/entities/venta.py
@@ -1,6 +1,6 @@
 import uuid
 
-from sqlalchemy import Boolean, Column, DateTime, String,Numeric,ForeignKey
+from sqlalchemy import Boolean, Column, DateTime, String, Numeric, ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
@@ -11,19 +11,20 @@ from src.database.config import Base
 class Venta(Base):
     __tablename__ = "tbl_Venta"
 
-    id_Venta = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True)
+    id_Venta = Column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, index=True
+    )
     fecha = Column(DateTime(timezone=True), server_default=func.now())
-    Precio_Venta = Column(Numeric(10,2), nullable=False)
+    Precio_Venta = Column(Numeric(10, 2), nullable=False)
     Metodo_Pago = Column(String(100), nullable=False)
-
 
     fecha_edicion = Column(DateTime(timezone=True), onupdate=func.now())
 
     id_usuario_crea = Column(
-        UUID(as_uuid=True), ForeignKey("tbl_usuarios.id_Usuarios"), nullable=False
+        UUID(as_uuid=True), ForeignKey("tbl_usuarios.id_Usuario"), nullable=False
     )
     id_usuario_edita = Column(
-        UUID(as_uuid=True), ForeignKey("tbl_usuarios.id_Usuarios"), nullable=True
+        UUID(as_uuid=True), ForeignKey("tbl_usuarios.id_Usuario"), nullable=True
     )
 
     usuario_crea = relationship("Usuario", foreign_keys=[id_usuario_crea])
